bc3989c8e287ef364e03e7cfb8919a99411c4858
Theodoros Maikantis <teomaik19@gmail.com>
2025-01-20 16:47:15 +0200

    Changed path pointing to user files
    changed from Absolute Path to Relative.

--- plumber_analytics_Skillab_KC.R
+++ plumber_analytics_Skillab_KC.R
@@ -1,7 +1,8 @@
 
 
 #path_user_files_all="C:/Users/kostas charm/Documents/Skillab/analytics_microservice/"
-path_user_files_all="C:/Users/zapfl/OneDrive/Documents/phd/Skillab/analytics_microservice/"
+#path_user_files_all="C:/Users/zapfl/OneDrive/Documents/phd/Skillab/analytics_microservice/"
+path_user_files_all="~/"
 
 
 library(plumber)
